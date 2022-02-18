3c75499cb5f916f9165d5e85f9fe92299d3edb4d
MishchenkoDaniil <97813066+MishchenkoDaniil@users.noreply.github.com>
2022-02-18 14:19:07 +0200

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "my_intance_type" {
-  type = string
-  description = "My instance type"
-  default = "t2.micro"
-}
-variable "private_subnet" {
-  type = list
-  default = ["10.0.1.0/24"]
-}
